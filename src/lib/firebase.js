61b57aa7e2eccafddf4df46dfae88d85fe88a2fe
SyedHuzaifaNazim <huzaifanazim154@gmail.com>
2024-09-09 12:59:51 +0500

    modified firebase ...

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -15,15 +15,7 @@ import dynamic from "next/dynamic";
 
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDAtEhyKjjOq0LIJp9LdyDHuNhpUE8stqM",
-  authDomain: "instagramclone-50ea1.firebaseapp.com",
-  projectId: "instagramclone-50ea1",
-  storageBucket: "instagramclone-50ea1.appspot.com",
-  messagingSenderId: "392623062530",
-  appId: "1:392623062530:web:b548b287591e0362d3f11b",
-  measurementId: "G-GQFQL5ZNF6"
-};
+
 
 
 const app = initializeApp(firebaseConfig);
