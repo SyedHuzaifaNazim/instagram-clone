12df1958502e85e9c48de8d867f139471991a8d5
SyedHuzaifaNazim <huzaifanazim154@gmail.com>
2024-10-21 00:48:00 +0500

    firebase configured

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,13 +5,12 @@ import { getAnalytics } from "firebase/analytics";
 import dynamic from "next/dynamic";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDAtEhyKjjOq0LIJp9LdyDHuNhpUE8stqM",
-  authDomain: "instagramclone-50ea1.firebaseapp.com",
-  projectId: "instagramclone-50ea1",
-  storageBucket: "instagramclone-50ea1.appspot.com",
-  messagingSenderId: "392623062530",
-  appId: "1:392623062530:web:b548b287591e0362d3f11b",
-  measurementId: "G-GQFQL5ZNF6"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 
@@ -24,7 +23,6 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
 
-<<<<<<< HEAD
 // const analytics = dynamic(() => import('firebase/analytics'), {
 //   ssr: false,
 // })();
